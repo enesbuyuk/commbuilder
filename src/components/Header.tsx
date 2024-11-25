4ec44bb455d7b3ba074100ce0b6b3f309d63d5b5
Enes Büyük <contact@enesbuyuk.com>
2024-11-25 21:56:54 +0300

    Add dynamic alt and title tag for classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,20 +5,15 @@ import LanguageSwitcher from "@/components/LanguageSwitcher";
 
 export function Header () {
     const tPage = useTranslations("Header");
+    const tGeneral = useTranslations("General");
   return (
       <header className="text-gray-600 body-font">
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href={"/"}
-                    title={"Istanbul University Computer Science Club"}>
+                    title={tGeneral("universityName")+" "+tGeneral("studentClubName")}>
                   {/* eslint-disable-next-line react/jsx-no-undef */}
-                  <Image src="/theme/logo.png" alt="Istanbul University Computer Science Club Logo" width={270}
+                  <Image src="/theme/logo.png" alt={tGeneral("universityName")+" "+tGeneral("studentClubName") + " Logo"} width={270}
                          height={45}/>
-                  {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round"*/}
-                  {/*     strokeLinejoin="round" strokeWidth="2"*/}
-                  {/*     className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">*/}
-                  {/*    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>*/}
-                  {/*</svg>*/}
-                  {/*<span className="ml-3 text-xl">Tailblocks</span>*/}
               </Link>
               <nav
                   className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
