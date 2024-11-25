8fe71b8353837a8519130fe07d34d70491051798
Enes Büyük <contact@enesbuyuk.com>
2024-11-25 22:28:49 +0300

    Update announcements page for multilanguage

--- src/app/[locale]/announcements/page.tsx
+++ src/app/[locale]/announcements/page.tsx
@@ -8,17 +8,20 @@ import {useTranslations} from "next-intl";
 //     description: "Stay up-to-date with the latest news and announcements from the Istanbul University Computer Science Club."
 // };
 export default function Page() {
-    const pageT = useTranslations("announcementsPage");
+    const pageT = useTranslations("AnnouncementsPage");
     return (
         <main>
             <section className="text-gray-600 body-font overflow-hidden">
-                <h1>{pageT("title")}</h1>
-                <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-20 bg-secondaryDark p-12 pt-20 text-white">
+                    <h1 className="text-3xl font-bold title-font mb-4 text-white tracking-widest">{pageT("title")}</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">{pageT("description")}</p>
+                </div>
+                <div className="container px-5 py-24 items-center mx-auto p-6 flex flex-col space-y-6">
                     <div className="-my-8 divide-y-2 divide-gray-100">
                         <div className="py-8 flex flex-wrap md:flex-nowrap">
                             <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                                 <span className="font-semibold title-font text-gray-700">EVENTS</span>
-                                <span className="mt-1 text-gray-500 text-sm">25 Novemver 2024</span>
+                                <span className="mt-1 text-gray-500 text-sm">25 November 2024</span>
                             </div>
                             <div className="md:flex-grow">
                                 <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">VODAFONE</h2>
