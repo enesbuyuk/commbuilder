00d16076f5bd5d5dfc26eee953c25d8886d0f522
Enes Buyuk <contact@enesbuyuk.com>
2024-12-05 12:18:50 +0300

    Add social media icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,80 @@
 import Link from "next/link";
+import Image from "next/image";
+import {useTranslations} from "next-intl";
+import React from "react";
 
 export default function Footer() {
-    return (
-        <footer className="body-font bg-primary text-white mt-24">
-            <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+    const year = (new Date()).getFullYear(); // I want to make a dynamic year because why not
+    const generalT = useTranslations("General");
 
-                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"
-                         fill="#fff">
-                        <path
-                            d="m325.2-77.02-62.9-25.87 57.13-137.59q-108.8-26.39-177.1-114.13-68.31-87.74-68.31-199.98v-174.08q0-65.92 46.54-111.73 46.54-45.82 112.24-45.82 17.33 0 33.31 3.72 15.99 3.72 31.93 10.8L544-769.67 390.35-712.8v71.45l463.04 296.61 40.24 197.89h-85.74l-41.11-84H556.54v156.83h-68.13v-156.83h-99.08L325.2-77.02Zm72.56-221.72h401.28l-100.52-64H397.36q-70.64 0-122.39-47.31-51.75-47.32-51.75-117.47h68.13q0 41.82 31.26 69.23 31.26 27.42 75.15 27.42H592.8L322.46-604.04v-124.63q0-36.89-26.37-63.15-26.36-26.27-63.73-26.27-37.36 0-63.9 25.93-26.55 25.92-26.55 63.49v174.32q0 106.59 74.7 181.1 74.69 74.51 181.15 74.51ZM232.98-698.2q-12.82 0-21.5-8.67-8.68-8.68-8.68-21.5 0-12.83 8.68-20.83 8.68-8 21.5-8t20.82 8q8 8 8 20.83 0 12.82-8 21.5-8 8.67-20.82 8.67Zm164.78 335.46Z"/>
-                    </svg>
-                    <span className="ml-3 text-xl text-white tracking-widest">IUCS.NET</span>
-                </a>
-                <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©
-                    2024 IU CS CLUB —
-                    <Link href="https://enesbuyuk.com" className="text-white hover:font-semibold ml-1 duration-300"
+    return (
+        <footer className="body-font bg-primary text-white">
+            <div className="container px-8 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                <Link
+                    className="flex title-font font-medium items-center md:justify-start justify-center text-white mb-4 md:mb-0"
+                    href={process.env.SITE_URL}
+                    title={generalT("universityName") + " " + generalT("studentClubName")}>
+                    <Image src="/theme/logo_footer.png"
+                           alt={generalT("universityName") + " " + generalT("studentClubName") + " Logo"} width={60}
+                           height={60}/>
+                </Link>
+                <p className="text-md text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 font-semibold">©
+                    {year} IU CS CLUB —
+                    <Link href="https://enesbuyuk.com" className="text-white hover:text-secondary hover:font-semibold ml-1 duration-300"
                           target="_blank">EnesBuyuk.com</Link>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <Link className="ml-3 text-secondary hover:text-white duration-300 align-middle" href={"https://x.com/iu_bbk"}>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-     viewBox="0 0 16 16"> <path
-    d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/></svg></Link>
 
-                    <Link className="ml-3 text-secondary hover:text-white duration-300 align-middle" href={"https://instagram.com/iu_bbk"}>
-        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-             className="w-5 h-5" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-      </Link>
     </span>
+
+                <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-4 p-4">
+                    <Link
+                        className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}
+                        href="https://instagram.com/iu_bbk" target="_blank"
+                        title={"Instagram"}>
+                        <svg className=""
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="currentColor"
+                             viewBox="0 0 448 512">
+                            <path
+                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                        </svg>
+                    </Link>
+                    <Link
+                        className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}                        href="https://www.linkedin.com/company/iubbk" target="_blank"
+                        title={"Linkedin"}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 448 512">
+                            <path
+                                d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/>
+                        </svg>
+                    </Link>
+                    <Link
+                        className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}                        href="https://chat.whatsapp.com/FgyXuaO2iJrLwtnSXpKUfe" target="_blank"
+                        title={"Whatsapp"}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 448 512">
+                            <path
+                                d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/>
+                        </svg>
+                    </Link>
+                    <Link
+                        className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}                        href="https://x.com/iu_bbk" target="_blank"
+                        title={"X (Social Media)"}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 512 512">
+                            <path
+                                d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
+                        </svg>
+                    </Link>
+                </div>
+
             </div>
         </footer>
 
