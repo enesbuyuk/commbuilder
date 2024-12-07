9514a361bb9113c66c780881a0c93e5fd4bf090f
Enes Buyuk <contact@enesbuyuk.com>
2024-12-07 07:01:47 +0300

    Enable dev. mode according to instructions in the comment lines

--- src/utils/startMongo.ts
+++ src/utils/startMongo.ts
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import {MongoClient, ServerApiVersion} from 'mongodb'
 
 if (!process.env.MONGODB_URI) {
     throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
@@ -20,7 +20,6 @@ if (process.env.NODE_ENV === 'development') {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
 
-    /*
     const globalWithMongo = global as typeof globalThis & {
         _mongoClientPromise?: Promise<MongoClient>
     }
@@ -29,12 +28,8 @@ if (process.env.NODE_ENV === 'development') {
         client = new MongoClient(uri, options)
         globalWithMongo._mongoClientPromise = client.connect()
     }
-    clientPromise = globalWithMongo._mongoClientPromise*/
-
-        // In production mode, it's best to not use a global variable.
-        client = new MongoClient(uri, options)
-        clientPromise = client.connect()
-    } else {
+    clientPromise = globalWithMongo._mongoClientPromise
+} else {
     // In production mode, it's best to not use a global variable.
     client = new MongoClient(uri, options)
     clientPromise = client.connect()
