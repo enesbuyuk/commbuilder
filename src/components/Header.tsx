b6babe6b04b3f089dedab4fa831890538959f83a
Enes Buyuk <contact@enesbuyuk.com>
2024-12-07 07:06:31 +0300

    Refactor navbar components: move variables and logic from header to navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,9 @@
-import React from 'react';
-import {useTranslations} from "next-intl";
-import {getPath} from "@/i18n/routing";
 import Navbar from "@/components/Navbar";
 
-export function Header({locale}) {
-    const translations = {
-        generalTranslations: useTranslations("General"),
-        pageTranslations: useTranslations("Header")
-    }
-
-    const menuItems = [
-        {href: `${process.env.SITE_URL}`, label: translations.pageTranslations("index")},
-        {href: "https://medium.iucs.net", label: "Medium", external: true},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/announcements', locale)}`, label: translations.pageTranslations("announcements")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/events', locale)}`, label: translations.pageTranslations("events")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/useful-links', locale)}`, label: translations.pageTranslations("links")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/gallery', locale)}`, label: translations.pageTranslations("gallery")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/about', locale)}`, label: translations.pageTranslations("about")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/contact', locale)}`, label: translations.pageTranslations("contact")},
-        {href: `${process.env.SITE_URL}/${locale}${getPath('/faq', locale)}`, label: translations.pageTranslations("faq")},
-    ];
-
+export default async function Header({locale}: {locale: string}) {
     return (
         <header className="text-gray-600 body-font">
-            <Navbar translations={{
-                pageTranslations: {
-                    index: translations.pageTranslations("index"),
-                    announcements: translations.pageTranslations("announcements"),
-                    events: translations.pageTranslations("events"),
-                    links: translations.pageTranslations("links"),
-                    gallery: translations.pageTranslations("gallery"),
-                    about: translations.pageTranslations("about"),
-                    contact: translations.pageTranslations("contact"),
-                    faq: translations.pageTranslations("faq"),
-                    joinTheClub: translations.pageTranslations("joinTheClub")
-                },
-                generalTranslations: {
-                    universityName: translations.generalTranslations("universityName"),
-                    studentClubName: translations.generalTranslations("studentClubName")
-                }
-            }} menuItems={menuItems}/>
+            <Navbar locale={locale}/>
         </header>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,62 +4,81 @@ import Image from "next/image";
 import {Menu, X} from "lucide-react";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
 import React, {useState} from "react";
+import {getPath} from "@/i18n/routing";
+import {useTranslations} from "next-intl";
 
-export default function Navbar({translations, menuItems}){
+export default function Navbar({locale}: {locale: string}){
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    const translations = {
+        generalTranslations: useTranslations("General"),
+        pageTranslations: useTranslations("Header")
+    }
+
+    const menuItems = [
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}`, label: translations.pageTranslations("index")},
+        {href: `${process.env.NEXT_PUBLIC_MEDIUM_URL}`, label: "Medium", external: true},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/announcements', locale)}`, label: translations.pageTranslations("announcements")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/events', locale)}`, label: translations.pageTranslations("events")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/useful-links', locale)}`, label: translations.pageTranslations("links")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/gallery', locale)}`, label: translations.pageTranslations("gallery")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/about', locale)}`, label: translations.pageTranslations("about")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/contact', locale)}`, label: translations.pageTranslations("contact")},
+        {href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/faq', locale)}`, label: translations.pageTranslations("faq")},
+    ];
+
     return (
-        <div className="container mx-auto flex flex-wrap px-4 py-5 items-center justify-between">
+        <div className="container mx-auto flex flex-wrap px-8 py-5 items-center justify-between gap-8">
             <Link
                 className="flex title-font font-medium items-center text-gray-900"
                 href="/"
-                title={`${translations.generalTranslations.universityName} ${translations.generalTranslations.studentClubName}`}
+                title={`${translations.generalTranslations("universityName")} ${translations.generalTranslations("studentClubName")}`}
             >
                 <Image
                     src="/theme/new_logo_no_bg.png"
-                    alt={`${translations.generalTranslations.universityName} ${translations.generalTranslations.studentClubName} Logo`}
-                    width={85}
-                    height={85}
+                    alt={`${translations.generalTranslations("universityName")} ${translations.generalTranslations("studentClubName")} Logo`}
+                    width={100}
+                    height={100}
                 />
             </Link>
 
             <button
-                className="md:hidden z-50"
+                className="absolute top-12 right-12 z-50 lg:hidden"
                 onClick={toggleMenu}
             >
-                {isMenuOpen ? <X size={24}/> : <Menu size={24}/>}
+                {isMenuOpen ? <X size={44}/> : <Menu size={44}/>}
             </button>
 
-            <nav className={`
-                    fixed inset-0 bg-white z-40 flex flex-col items-center justify-center 
-                    md:static md:flex md:flex-row md:bg-transparent md:z-0
+            <nav className={`fixed inset-0 bg-white z-40 flex flex-col items-center justify-center 
+                    lg:static lg:flex lg:flex-row lg:bg-transparent lg:z-0
                     transition-all duration-300 ease-in-out
                     ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}
-                    md:translate-x-0
+                    lg:translate-x-0
+                    flex-grow
                 `}>
-                <div className="flex flex-col md:flex-row items-center">
-                    {menuItems.map((item) => (
-                        <Link
-                            key={item.href}
-                            className="my-2 md:my-0 md:mr-5 text-black hover:text-primary font-semibold text-lg"
-                            href={item.href}
-                            {...(item.external ? {target: "_blank"} : {})}
-                            onClick={toggleMenu}
-                        >{item.label}</Link>
-                    ))}
-
-                    <div className="flex flex-col md:flex-row items-center">
+                <div className="flex flex-col lg:flex-row justify-between items-center w-full px-4">
+                    <div className="flex flex-col lg:flex-row items-center">
+                        {menuItems.map((item) => (
+                            <Link
+                                key={item.href}
+                                className="my-2 lg:my-0 lg:mr-5 text-black hover:text-primary font-semibold text-lg"
+                                href={item.href}
+                                {...(item.external ? {target: "_blank"} : {})}
+                                onClick={toggleMenu}
+                            >{item.label}</Link>
+                        ))}
+                    </div>
+                    <div className="flex flex-col lg:flex-row items-center">
                         <LanguageSwitcher/>
                         <Link
-                            className="inline-flex items-center bg-primary hover:bg-secondary text-gray-300 hover:text-white border-0 focus:outline-none rounded text-base mt-4 md:mt-0 duration-300 px-4 py-2"
-                            href={`${translations.pageTranslations.joinTheClub}`}
+                            className="inline-flex items-center bg-primary hover:bg-secondary text-gray-300 hover:text-white border-0 focus:outline-none rounded text-base mt-4 lg:mt-0 duration-300 px-4 py-2"
+                            href={`${process.env.NEXT_PUBLIC_SITE_URL}/${locale}${getPath('/join-the-club', locale)}`}
                             onClick={toggleMenu}
-                        >
-                            {translations.pageTranslations.joinTheClub}
+                        >{translations.pageTranslations("joinTheClub")}
                             <svg
                                 fill="none"
                                 stroke="currentColor"
@@ -75,6 +94,7 @@ export default function Navbar({translations, menuItems}){
                     </div>
                 </div>
             </nav>
+
         </div>
     )
 }
