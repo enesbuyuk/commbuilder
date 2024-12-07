9acddfb610ea91da6d00ce01ac4c1170dc8c8561
Enes Buyuk <contact@enesbuyuk.com>
2024-12-07 06:46:25 +0300

    Add the interface of announcement

---
+++ src/types/Announcement.ts
@@ -0,0 +1,10 @@
+export interface Announcement {
+    _id: string;
+    announcement_type: string;
+    announcement_title: string;
+    announcement_description: string;
+    announcement_title_tr: string;
+    announcement_description_tr: string;
+    announcement_date: string;
+    announcement_url: string;
+}
