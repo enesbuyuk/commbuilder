3c743a58cba909cd3381942bd04e12755dfb86c1
Enes Buyuk <contact@enesbuyuk.com>
2024-12-22 04:40:24 +0300

    Feat: Add metadataBase & alternates for multilanguage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,19 @@ type Props = {
     children: ReactNode;
 };
 
+export async function generateMetadata() {
+    return {
+        metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!).origin,
+        alternates: {
+            canonical: '/',
+            languages: {
+                'en-US': '/en',
+                'tr-TR': '/tr',
+            },
+        },
+    };
+}
+
 export default function RootLayout({children}: Props) {
     return children;
 }
