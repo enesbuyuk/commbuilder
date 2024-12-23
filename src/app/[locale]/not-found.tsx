6bcfcf7f96103ff747b36f2dabf4a011f625a83e
Enes Buyuk <contact@enesbuyuk.com>
2024-12-23 23:00:12 +0300

    Refactor: Remove notfoundpage component and merge them

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,1 +1,18 @@
-export {default} from '@/components/NotFoundPage';
+import PageLayout from '@/components/PageLayout';
+import {getTranslations, setRequestLocale} from "next-intl/server";
+
+export default async function notFound({ params }: { params: Promise<{ locale: string }> }) {
+    // why is this not working?
+    // const {locale} = await params;
+    // setRequestLocale(locale);
+
+    const translations = {
+        pageTranslations: await getTranslations("NotFoundPage")
+    }
+
+    return (
+        <PageLayout locale={"en"} title={translations.pageTranslations('title')} description={translations.pageTranslations("description")}>
+            <p className="max-w-[460px]">{translations.pageTranslations('description')}</p>
+        </PageLayout>
+    );
+}
