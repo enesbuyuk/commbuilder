a04a95c3f20d56f704b012de10b0abff4f147e77
Enes Buyuk <contact@enesbuyuk.com>
2024-12-25 18:30:39 +0300

    Feat: Add mail link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,10 +31,22 @@ export default async function Footer() {
                 <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-4 p-4">
                     <Link
                         className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}
+                        href="mailto:info@iucs.net"
+                        title={"Mail"}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 448 512">
+                            <path
+                                d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
+                        </svg>
+                    </Link>
+                    <Link
+                        className={"[&>svg]:h-5 [&>svg]:w-5 flex items-center justify-center text-white hover:text-secondary transition duration-300"}
                         href="https://instagram.com/iu_bbk" target="_blank"
                         title={"Instagram"}>
-                        <svg className=""
-                             xmlns="http://www.w3.org/2000/svg"
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
                              fill="currentColor"
                              viewBox="0 0 448 512">
                             <path
@@ -78,6 +90,5 @@ export default async function Footer() {
 
             </div>
         </footer>
-
     )
 }
