5f74da0d34498f876790bb1c9b03a9ac032a3de8
Enes Buyuk <contact@enesbuyuk.com>
2025-03-12 02:53:19 +0300

    refactor: change sorting according to announcement date and add clientPromise

--- src/app/api/announcements/route.ts
+++ src/app/api/announcements/route.ts
@@ -1,16 +1,16 @@
-import connect from "@/utils/startMongo";
 import { NextResponse } from "next/server";
+import clientPromise from "@/utils/startMongo";
 
 export async function GET(request: Request) {
     try {
         const limitParam = new URL(request.url).searchParams.get("limit");
         const limit = limitParam ? parseInt(limitParam) : 100; // Default limit
 
-        const client = await connect;
+        const client = await clientPromise;
         const db = client.db(process.env.MONGODB_DB_NAME);
         const collection = db.collection("announcements");
 
-        const cursor = collection.find().sort({ date: -1 }).limit(limit);
+        const cursor = collection.find().sort({ announcement_date: -1 }).limit(limit);
         const data = await cursor.toArray();
 
         return NextResponse.json(data, { status: 200 });
