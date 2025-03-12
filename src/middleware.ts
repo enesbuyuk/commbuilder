b985e5c5728f7493813aabb781ef6ee20c5e8372
Enes Buyuk <contact@enesbuyuk.com>
2025-03-12 03:48:23 +0300

    feat: enhance middleware with JWT authentication for admin panel access

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,39 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
-export default createMiddleware(routing);
+// next-intl middleware
+const intlMiddleware = createMiddleware(routing);
+
+export function middleware(req: NextRequest) {
+    // firstly check internationalization middleware
+    const intlResponse = intlMiddleware(req);
+    if (intlResponse) return intlResponse;
+
+    // admin panel pages jwt checking middleware
+    if (req.nextUrl.pathname.startsWith("/admin")) {
+        const token = req.cookies.get("token")?.value;
+        if (!token) {
+            return NextResponse.redirect(new URL("/admin/sign-in", req.url));
+        }
+
+        try {
+            const secret = process.env.JWT_SECRET!;
+            jwt.verify(token, secret);
+        } catch (error) {
+            return NextResponse.redirect(new URL("/admin/sign-in", req.url));
+        }
+    }
+
+    return NextResponse.next();
+}
 
 export const config = {
     matcher: [
-        '/',
-        '/(tr|en)/:path*', // (e.g. `/pathnames` -> `/en/pathnames`)
-        '/((?!_next|api|admin|opengraph-image|_vercel|.*\\..*).*)' // API, static fiels routes are excluded
-    ]
+        "/",
+        "/(tr|en)/:path*",
+        '/((?!_next|api|opengraph-image|_vercel|.*\\..*).*)', // API, static fiels routes are excluded
+        "/admin/:path*", // admin pages
+    ],
 };
