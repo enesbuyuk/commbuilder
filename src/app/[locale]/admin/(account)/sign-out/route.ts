1f4a8de2d06e8b7e8cb74ee3caa6d016f4970690
Enes Buyuk <contact@enesbuyuk.com>
2025-03-14 04:32:49 +0300

    refactor: remove unnecessary comment in sign-out route for clarity.

--- src/app/[locale]/admin/(account)/sign-out/route.ts
+++ src/app/[locale]/admin/(account)/sign-out/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { getLocale } from "next-intl/server";
 
 export async function GET() {
-    const locale = await getLocale();  // Kullanıcının dilini al
+    const locale = await getLocale();
     const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/admin/sign-in`);
 
     response.cookies.set('token', '', {
