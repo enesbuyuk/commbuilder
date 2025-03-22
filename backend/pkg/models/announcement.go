1502b32ca1c47b8cdcf315e62362203d1d12b3e2
Enes Buyuk <contact@enesbuyuk.com>
2025-03-22 07:49:28 +0300

    refactor: update BSON and JSON field names for consistency in models

--- backend/pkg/models/announcement.go
+++ backend/pkg/models/announcement.go
@@ -5,11 +5,10 @@ import (
 )
 
 type AnnouncementModel struct {
-	ID                      primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	AnnouncementType        string             `bson:"announcement_type" json:"announcement_type"`
-	AnnouncementTitle       map[string]string  `bson:"announcement_title" json:"announcement_title"`
-	AnnouncementDescription map[string]string  `bson:"announcement_description" json:"announcement_description"`
-	AnnouncementDate        string             `bson:"announcement_date" json:"announcement_date"`
-	AnnouncementURL         string             `bson:"announcement_url" json:"announcement_url"`
-	EventType               map[string]string  `bson:"event_type" json:"event_type"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Type        string             `bson:"type" json:"type"`
+	Title       map[string]string  `bson:"title" json:"title"`
+	Description map[string]string  `bson:"description" json:"description"`
+	Date        string             `bson:"date" json:"date"`
+	URL         string             `bson:"url" json:"url"`
 }
--- backend/pkg/models/event.go
+++ backend/pkg/models/event.go
@@ -6,9 +6,9 @@ import (
 
 type EventModel struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Type        map[string]string  `bson:"event_type" json:"event_type"`
-	Title       map[string]string  `bson:"event_title" json:"event_title"`
-	Description map[string]string  `bson:"event_description" json:"event_description"`
-	Date        string             `bson:"event_date" json:"event_date"`
-	URL         string             `bson:"event_url" json:"event_url"`
+	Type        map[string]string  `bson:"type" json:"type"`
+	Title       map[string]string  `bson:"title" json:"title"`
+	Description map[string]string  `bson:"description" json:"description"`
+	Date        string             `bson:"date" json:"date"`
+	URL         string             `bson:"url" json:"url"`
 }
