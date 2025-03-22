b9ac4c9a9b9c16757bfa1d0a6eeb5116fd66fd9c
Enes Buyuk <contact@enesbuyuk.com>
2025-03-22 07:47:27 +0300

    feat: add limit validation and sorting for announcements and events queries

--- backend/pkg/handlers/events.go
+++ backend/pkg/handlers/events.go
@@ -6,6 +6,8 @@ import (
 	"github.com/enesbuyuk/university-student-club-website/pkg/models"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"strconv"
 	"time"
 )
 
@@ -14,7 +16,15 @@ func GetEvents(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cursor, err := config.DB.Collection("events").Find(ctx, bson.M{})
+	limit, err := strconv.Atoi(c.Query("limit", "100"))
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid limit value"})
+	}
+	if limit > 200 {
+		return c.Status(400).JSON(fiber.Map{"error": "Limit value is too high"})
+	}
+
+	cursor, err := config.DB.Collection("events").Find(ctx, bson.M{}, options.Find().SetSort(bson.M{"date": -1}).SetLimit(int64(limit)))
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"error": "Data could not be retrieved"})
 	}
