e362f9dbd85cbba707aa1f6e20e128c77ed5420b
Enes Buyuk <contact@enesbuyuk.com>
2025-03-22 06:58:41 +0300

    refactor: add database disconnection error handling and use environment port

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
 	"github.com/enesbuyuk/university-student-club-website/pkg/config"
 	routes2 "github.com/enesbuyuk/university-student-club-website/pkg/routes"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
+	"os"
 )
 
 func main() {
 	db := config.ConnectDB()
-	defer db.Client().Disconnect(nil)
+	defer func(client *mongo.Client, ctx context.Context) {
+		err := client.Disconnect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db.Client(), nil)
 
 	app := fiber.New()
 
@@ -17,5 +25,5 @@ func main() {
 	routes2.EventsRoutes(app)
 	routes2.AuthRoutes(app)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(os.Getenv("BE_PORT")))
 }
