ab890465adee43ce5e8246570124d11961c8e851
Enes Buyuk <contact@enesbuyuk.com>
2025-03-25 01:00:29 +0300

    fix: solve authentication route by removing unused middleware dependency

--- backend/pkg/routes/auth.go
+++ backend/pkg/routes/auth.go
@@ -2,11 +2,10 @@ package routes
 
 import (
 	"github.com/enesbuyuk/university-student-club-website/pkg/handlers"
-	"github.com/enesbuyuk/university-student-club-website/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func AuthRoutes(app *fiber.App) {
 	app.Get("/auth", handlers.GetAuth)
-	app.Post("/auth", middleware.JwtMiddleware, handlers.PostAuth)
+	app.Post("/auth", handlers.PostAuth)
 }
