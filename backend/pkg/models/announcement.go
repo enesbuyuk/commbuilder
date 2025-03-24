00172dc018d30075a3bf83815ac670759cc6e329
Enes Buyuk <contact@enesbuyuk.com>
2025-03-24 17:11:53 +0300

    refactor: change type field to a map for better multilingual support

--- backend/pkg/models/announcement.go
+++ backend/pkg/models/announcement.go
@@ -6,9 +6,8 @@ import (
 
 type AnnouncementModel struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Type        string             `bson:"type" json:"type"`
+	Type        map[string]string  `bson:"type" json:"type"`
 	Title       map[string]string  `bson:"title" json:"title"`
 	Description map[string]string  `bson:"description" json:"description"`
 	Date        string             `bson:"date" json:"date"`
-	URL         string             `bson:"url" json:"url"`
 }
