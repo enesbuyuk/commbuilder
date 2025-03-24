8ce77ac183f32ed492b53a92f96eed4bceb2ca0a
Enes Buyuk <contact@enesbuyuk.com>
2025-03-25 00:49:27 +0300

    feat: add JWT middleware to POST routes for announcements and events

--- backend/pkg/routes/announcements.go
+++ backend/pkg/routes/announcements.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"github.com/enesbuyuk/university-student-club-website/pkg/handlers"
+	"github.com/enesbuyuk/university-student-club-website/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func AnnouncementRoutes(app *fiber.App) {
 	app.Get("/announcements", handlers.GetAnnouncements)
-	app.Post("/announcements", handlers.PostAnnouncements)
+	app.Post("/announcements", middleware.JwtMiddleware, handlers.PostAnnouncements)
 }
--- backend/pkg/routes/events.go
+++ backend/pkg/routes/events.go
@@ -2,9 +2,11 @@ package routes
 
 import (
 	"github.com/enesbuyuk/university-student-club-website/pkg/handlers"
+	"github.com/enesbuyuk/university-student-club-website/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func EventsRoutes(app *fiber.App) {
 	app.Get("/events", handlers.GetEvents)
+	app.Post("/events", middleware.JwtMiddleware, handlers.PostEvents)
 }
