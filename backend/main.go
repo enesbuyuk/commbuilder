e732223fa27861b8e6da787e55ba28e81ad23a38
Enes Buyuk <contact@enesbuyuk.com>
2025-03-25 01:04:17 +0300

    feat: implement server setup and middleware for the backend application

--- backend/main.go
+++ backend/main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"context"
 	"github.com/enesbuyuk/university-student-club-website/pkg/config"
-	routes2 "github.com/enesbuyuk/university-student-club-website/pkg/routes"
-	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/enesbuyuk/university-student-club-website/pkg/server"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	db := config.ConnectDB()
-	defer func(client *mongo.Client, ctx context.Context) {
-		err := client.Disconnect(ctx)
-		if err != nil {
-			log.Fatal(err)
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := db.Client().Disconnect(ctx); err != nil {
+			log.Println("Error disconnecting from database:", err)
+		} else {
+			log.Println("Database disconnected successfully")
 		}
-	}(db.Client(), nil)
+	}()
 
-	app := fiber.New()
+	app := server.SetupServer()
 
-	routes2.AnnouncementRoutes(app)
-	routes2.EventsRoutes(app)
-	routes2.AuthRoutes(app)
-
-	log.Fatal(app.Listen(os.Getenv("BE_PORT")))
+	if err := app.Listen(":" + os.Getenv("BE_PORT")); err != nil {
+		log.Fatal(err)
+	}
 }
