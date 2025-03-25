25be05fbc2108e1257d4e92b2f3cd1223d1f00d6
Enes Buyuk <contact@enesbuyuk.com>
2025-03-25 05:14:13 +0300

    refactor: add pageName constant to pass as a prop to MediumArticles and LastEvents

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -4,6 +4,8 @@ import LastEvents from "@/components/LastEvents";
 import Header from "@/components/Header";
 import React from "react";
 
+const pageName = "index";
+
 export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
     const {locale} = await params;
     setRequestLocale(locale);
@@ -12,8 +14,8 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
         <>
             <Header locale={locale} indexHero={true}/>
             <main className="flex flex-col items-center justify-center w-full overflow-hidden">
-                <MediumArticles/>
-                <LastEvents locale={locale}/>
+                <MediumArticles locale={locale} pageName={pageName}/>
+                <LastEvents locale={locale} pageName={pageName}/>
             </main>
         </>
     )
