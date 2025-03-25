dc3a5851e82fe9dab9824617b3203b2dc92ee363
Enes Buyuk <contact@enesbuyuk.com>
2025-03-25 03:59:58 +0300

    feat: add metadata retrieval function for localized page content

---
+++ frontend/src/lib/metadata.ts
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import {getPath, Pathnames} from "@/i18n/routing";
+
+export async function getMetadata(locale: string, page: string): Promise<Metadata> {
+    const [generalTranslations, pageTranslations] = await Promise.all([
+        getTranslations({locale, namespace:`metadata.general`}),
+        getTranslations({locale, namespace:`metadata.${page}`})
+    ]);
+
+    if(page.includes("--")) {
+        page = page.replace("--", "/");
+    }
+    return {
+        title: pageTranslations('title') + generalTranslations("titleSuffix"),
+        description: pageTranslations('description'),
+        keywords: pageTranslations('keywords'),
+        openGraph: {
+            siteName: generalTranslations('title'),
+            title: pageTranslations('title'),
+            description: pageTranslations('description'),
+            type: 'website'
+        },
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/${getPath(`/` + page as Pathnames, locale)}`,
+            languages: {
+                en: `${process.env.NEXT_PUBLIC_SITE_URL}/en/${getPath(`/` + page as Pathnames, "en")}`,
+                tr: `${process.env.NEXT_PUBLIC_SITE_URL}/tr/${getPath(`/` + page as Pathnames, "tr")}`,
+                //de: `${process.env.NEXT_PUBLIC_SITE_URL}/de/${getPath(`/` + page as Pathnames, "de")}`,
+            }
+        }
+    }
+}
