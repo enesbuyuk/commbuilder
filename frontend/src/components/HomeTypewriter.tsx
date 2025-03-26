80bc465ff9af4ca6587285c9f007446b2783720e
Enes Buyuk <contact@enesbuyuk.com>
2025-03-26 03:38:40 +0300

    fix: "Error: Bail out to client-side rendering: next/dynamic"

--- frontend/src/components/HomeTypewriter.tsx
+++ frontend/src/components/HomeTypewriter.tsx
@@ -1,40 +1,20 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { useTranslations } from "next-intl";
-import { useEffect, useState } from "react";
-
-const Typewriter = dynamic(() => import("typewriter-effect"), {
-    ssr: false,
-});
+import { Typewriter } from "react-simple-typewriter";
 
 export default function HomeTypewriter() {
     const t = useTranslations("pages.index.slogans");
-    const [slogans, setSlogans] = useState<string[]>([]);
-
-    useEffect(() => {
-        const slogansData = [
-            t("slogan1"),
-            t("slogan2"),
-            t("slogan3"),
-        ];
-        setSlogans(slogansData);
-    }, [t]);
 
     return (
         <Typewriter
-            options={{ loop: true }}
-            onInit={(typewriter) => {
-                slogans.forEach((text, index) => {
-                    typewriter
-                        .typeString(text)
-                        .pauseFor(2500)
-                        .deleteAll();
-                    if (index === slogans.length - 1) {
-                        typewriter.start();
-                    }
-                });
-            }}
+            words={[t("slogan1"), t("slogan2"), t("slogan3")]}
+            loop={true}
+            cursor
+            cursorStyle="_"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
         />
     );
 }
