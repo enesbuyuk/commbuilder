a746cfc177dcbedb548c04f4caf4d447ad924a57
Enes Buyuk <contact@enesbuyuk.com>
2025-03-28 19:27:48 +0300

    refactor: remove BaseLayout component and integrate its functionality directly

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,19 +1,46 @@
 import { notFound } from 'next/navigation';
-import {setRequestLocale} from 'next-intl/server';
-import BaseLayout from '@/components/BaseLayout';
+import {getMessages, getTranslations, setRequestLocale} from 'next-intl/server';
 import { routing } from '@/i18n/routing';
 import React from "react";
-import {getMetadata} from "@/lib/metadata";
+import {NextIntlClientProvider} from "next-intl";
+import Footer from "@/components/Footer";
 
 export function generateStaticParams() {
     return routing.locales.map((locale) => ({ locale }));
 }
 
-const pageName = "index";
-
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
     const { locale } = await params;
-    return getMetadata(locale, pageName);
+
+    const metadataTranslations = await getTranslations({
+        locale,
+        namespace: "metadata.index"
+    })
+
+    return {
+        metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+        title:{
+            default: metadataTranslations("title"),
+            template: "%s | " + metadataTranslations("title")
+        },
+        description: metadataTranslations("description"),
+        keywords: metadataTranslations("keywords"),
+        openGraph:{
+            siteName: metadataTranslations("title"),
+            title: {
+                default: metadataTranslations("title"),
+                template: "%s | " + metadataTranslations("title")
+            },
+            type: "website",
+        },
+        alternates:{
+            canonical: process.env.NEXT_PUBLIC_SITE_URL,
+            languages: {
+                en: process.env.NEXT_PUBLIC_SITE_URL + "/en",
+                tr: process.env.NEXT_PUBLIC_SITE_URL + "/tr",
+            }
+        }
+    }
 }
 
 export default async function LocaleLayout({children, params}: {children: React.ReactNode; params: Promise<{locale:string}> }) {
@@ -27,5 +54,16 @@ export default async function LocaleLayout({children, params}: {children: React.
     // Enable static rendering
     setRequestLocale(locale);
 
-    return <BaseLayout locale={locale}>{children}</BaseLayout>;
+    const messages = await getMessages();
+
+    return (
+        <html lang={locale}>
+            <body className="text-gray-900 bg-gray-100 font-roboto antialiased">
+                <NextIntlClientProvider messages={messages} locale={locale}>
+                    {children}
+                    <Footer/>
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
