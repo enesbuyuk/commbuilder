5f70e25d54c8ca609ffb00f879187fa74d75f0c9
Enes Buyuk <contact@enesbuyuk.com>
2025-03-28 20:23:18 +0300

    refactor: update AdminHeader to use async translations for improved performance

--- frontend/src/components/admin/AdminHeader.tsx
+++ frontend/src/components/admin/AdminHeader.tsx
@@ -1,37 +1,39 @@
-'use client';
 import Link from "next/link";
 import React from "react";
 import {getPath} from "@/i18n/routing";
-import {useTranslations} from "next-intl";
 import Image from "next/image";
+import { getTranslations } from "next-intl/server";
 
-export default function AdminHeader({locale}:{locale:string}){
-    const translations = {
-        generalTranslations: useTranslations("General"),
-        pageTranslations: useTranslations("AdminPage")
-    }
+const pageName = "admin";
+
+export default async function AdminHeader({locale}:{locale:string}){
+
+    const [generalTranslations, contentTranslations] = await Promise.all([
+        getTranslations({locale, namespace:`general`}),
+        getTranslations({locale, namespace:`pages.${pageName}`})
+    ]);
 
     return (
         <header className="body-font bg-primary text-white p-6 flex justify-between items-center">
             <Link
                 className="flex title-font font-medium items-center text-gray-900 justify-around"
-                href={process.env.NEXT_PUBLIC_SITE_URL+"/"+locale+"/admin"}
-                title={`${translations.generalTranslations("universityName")} ${translations.generalTranslations("studentClubName")}`}
+                href={`/${locale}/admin`}
+                title={`${generalTranslations("universityName")} ${generalTranslations("studentClubName")}`}
             >
                 <Image
                     src="/theme/logo_footer.png"
-                    alt={`${translations.generalTranslations("universityName")} ${translations.generalTranslations("studentClubName")} Logo`}
+                    alt={`${generalTranslations("universityName")} ${generalTranslations("studentClubName")} Logo`}
                     width={70}
                     height={70}
                 />
-                <span className="ml-3 text-xl font-extrabold text-white">{translations.generalTranslations("universityName")}<br/>{translations.generalTranslations("studentClubName")} Admin Panel</span>
+                <span className="ml-3 text-xl font-extrabold text-white">{generalTranslations("universityName")}<br/>{generalTranslations("studentClubName")} Admin Panel</span>
             </Link>
             <nav>
                 <ul className="flex space-x-6">
-                    <li><Link className={"border-r-2 border-white pr-2"} href={process.env.NEXT_PUBLIC_SITE_URL+"/"+locale} target="_blank" >Web Site Home</Link></li>
-                    <li><Link href={`/${locale}${getPath('/admin/dashboard', locale)}`} className="hover:text-gray-400 duration-300">{translations.pageTranslations("dashboard")}</Link></li>
-                    <li><Link href={`/${locale}${getPath('/admin/settings', locale)}`} className="hover:text-gray-400 duration-300">{translations.pageTranslations("settings")}</Link></li>
-                    <li><Link href={`/${locale}${getPath('/admin/sign-out', locale)}`} className="hover:text-gray-400 duration-300">{translations.pageTranslations("signOut")}</Link></li>
+                    <li><Link className={"border-r-2 border-white pr-2"} href={`/${locale}`} target="_blank" >Web Site Home</Link></li>
+                    <li><Link href={`/${locale}${getPath('/admin/dashboard', locale)}`} className="hover:text-gray-400 duration-300">{contentTranslations("dashboard")}</Link></li>
+                    <li><Link href={`/${locale}${getPath('/admin/settings', locale)}`} className="hover:text-gray-400 duration-300">{contentTranslations("settings")}</Link></li>
+                    <li><Link href={`/${locale}${getPath('/admin/sign-out', locale)}`} className="hover:text-gray-400 duration-300">{contentTranslations("signOut")}</Link></li>
                 </ul>
             </nav>
         </header>
