d8e81c831e2fcb94335f14e453e02327f88848f4
Enes Buyuk <contact@enesbuyuk.com>
2025-03-29 20:31:02 +0300

    feat: add login handling function with cookie management and redirection

--- frontend/src/app/[locale]/admin/(account)/sign-in/page.tsx
+++ frontend/src/app/[locale]/admin/(account)/sign-in/page.tsx
@@ -1,8 +1,7 @@
 import {getTranslations, setRequestLocale} from "next-intl/server";
 import Image from "next/image";
-import { redirect } from "next/navigation";
-import {cookies} from "next/headers";
 import {getMetadata} from "@/lib/metadata";
+import {handleLogin} from "@/lib/auth/auth";
 
 const pageName = "admin";
 
@@ -72,32 +71,4 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
             </div>
         </div>
     );
-}
-
-export async function handleLogin(formData: FormData) {
-    "use server";
-    const username = formData.get("username") as string;
-    const password = formData.get("password") as string;
-
-    const response = await fetch(`${process.env.BACKEND_URL}/auth`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-    });
-
-    if (response.ok) {
-        const data = await response.json();
-        const token = data.token;
-
-        (await cookies()).set("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            path: "/",
-            maxAge: 60 * 60 * 24 * 7,
-        });
-
-        redirect(`/admin`);
-    } else {
-        console.error("Login failed. Please try again.");
-    }
 }
