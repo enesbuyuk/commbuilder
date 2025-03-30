cb148cc3bdd394dd02a74a0194797a917b5e493c
Enes Buyuk <contact@enesbuyuk.com>
2025-03-30 20:55:54 +0300

    fix: log warning and continue if .env file is not found

--- backend/pkg/config/environment.go
+++ backend/pkg/config/environment.go
@@ -2,11 +2,12 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"log"
 )
 
 func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		panic("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		// Log the warning but continue execution
+		log.Println("Warning: .env file not found, using environment variables")
 	}
 }
