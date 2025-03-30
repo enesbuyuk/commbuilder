aa7da92018e38584d4045b20899c1fe3c9d07ad0
Enes Buyuk <contact@enesbuyuk.com>
2025-03-30 22:10:10 +0300

    fix: Type '{ id: string; }' is missing the following properties from type 'Promise<any>'

--- frontend/src/app/[locale]/admin/(dashboard)/announcements/[id]/remove/page.tsx
+++ frontend/src/app/[locale]/admin/(dashboard)/announcements/[id]/remove/page.tsx
@@ -1,3 +1,4 @@
-export default async function Page({ params }: { params: { id: string } }) {
-    return <div>My Post: {params.id}</div>;
+export default async function Page({params,}: { params: Promise<{ id: string }> }) {
+    const { id } = await params
+    return <div>My Post: {id}</div>
 }
